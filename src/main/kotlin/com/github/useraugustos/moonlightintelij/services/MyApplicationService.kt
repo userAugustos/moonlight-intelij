fc3746e23eeb5457592befcbbda6af64e38ba2f9
GitHub Action <action@github.com>
2023-02-08 18:53:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/useraugustos/moonlightintelij/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/useraugustos/moonlightintelij/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.useraugustos.moonlightintelij.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.useraugustos.moonlightintelij.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/useraugustos/moonlightintelij/services/MyProjectService.kt
+++ src/main/kotlin/com/github/useraugustos/moonlightintelij/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.useraugustos.moonlightintelij.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.useraugustos.moonlightintelij.MyBundle
 
 class MyProjectService(project: Project) {
 
